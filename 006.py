78cb9ef4f1aa30ff2ee62e4d2dc47a4f830f88ad
Emily Carpenter <ecarpenter23@gmail.com>
2024-03-25 18:27:34 -0400

    improve 006

--- 006.py
+++ 006.py
@@ -3,10 +3,8 @@ def six():
     Find the difference between the sum of the squares of the first
     one hundred natural numbers and the square of the sum.
     """
-    summa = 0
-    for i in range(101):
-        summa = summa + (i ** 2)
-    sq_sums = ((100 * 101) / 2) ** 2
-    return abs(sq_sums - summa)
+    summa = sum([i ** 2 for i in range(101)])
+    sq_summa = ((100 * 101) / 2) ** 2
+    return abs(sq_summa - summa)
 
 print(six())
